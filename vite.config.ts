7374600698e01e4cedc5861df78da2d073da75d5
pedroalunouninter <1992822@alunouninter.com>
2026-04-25 00:22:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/PROJETO-MULTIDISCIPLINAR-FRONT-END---Raizes-do-Nordeste---V1/",
   plugins: [react()],
 });
